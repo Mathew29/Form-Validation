a949e6d79c9f52a6f7c2a521ce7d1e2314ae3787
Mathew <mwakre29@gmail.com>
2021-05-12 23:08:17 -0700

    make a state obj and set state

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from "react";
 
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  passwordCheck: "",
+};
+
 const RegisterForm = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordCheck, setPasswordCheck] = useState("");
+  const [formState, setFormState] = useState(initialState);
+
+  const handleInputChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(firstName, lastName, email, password, passwordCheck);
+    if (formState.password !== formState.passwordCheck)
+      return console.log("passwords do not match");
+
+    console.log(formState);
+    setFormState(initialState);
   };
 
   return (
@@ -19,35 +34,35 @@ const RegisterForm = () => {
           type="text"
           name="firstName"
           placeholder="First Name..."
-          onChange={(event) => setFirstName(event.target.value)}
+          onChange={handleInputChange}
           required
         />
         <input
           type="text"
           name="lastName"
           placeholder="Last Name..."
-          onChange={(event) => setLastName(event.target.value)}
+          onChange={handleInputChange}
           required
         />
         <input
           type="email"
           name="email"
           placeholder="Email..."
-          onChange={(event) => setEmail(event.target.value)}
+          onChange={handleInputChange}
           required
         />
         <input
           type="password"
           name="password"
           placeholder="Password..."
-          onChange={(event) => setPassword(event.target.value)}
+          onChange={handleInputChange}
           required
         />
         <input
           type="password"
           name="passwordCheck"
           placeholder="Check Password"
-          onChange={(event) => setPasswordCheck(event.target.value)}
+          onChange={handleInputChange}
           required
         />
         <button>Submit</button>
